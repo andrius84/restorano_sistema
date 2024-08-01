45a0ffa1db6e78a8905ad6ca8230c3e5b837e0eb
Andrius Ašmanavičius <andrius.asmanavicius@gmail.com>
2024-08-01 22:34:40 +0300

    Add project files.

---
+++ Projektas_restorano_sistema/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Projektas_restorano_sistema
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
